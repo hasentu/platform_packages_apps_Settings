7d86a330a229f09b8b7d2ab0a7dbbaccc2385ef8
donghaomin <dhmcharles@gmail.com>
2014-03-29 12:07:42 +0100

    [2/2] LED customization
    * Add LED customization.
    * Add global and per-app settings.
    * Set default LED color as white.
    * Remove stock on/off checkbox and add an dedicated category.
    * Comment out dead code from display settings.
    
    [2/2] 个性化LED
    
    * 添加LED个性化.
    * 添加全局和各个应用的设置.
    * 设置默认LED颜色为白色.
    * 删除原生开/关勾选框, 并添加一个独立分类.
    * 将显示设置中的无用代码加为评论.
    
    [1/2] in platform_frameworks_base
    
    Thanks to CyanogenMod for the original implementation.
    感谢CyanogenMod原创实现此功能.

--- src/com/android/settings/biantai/SystemUiSettings.java
+++ src/com/android/settings/biantai/SystemUiSettings.java
@@ -42,6 +42,7 @@ package com.android.settings.biantai;
 
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -50,11 +51,27 @@ public class SystemUiSettings extends SettingsPreferenceFragment implements Pref
 
     private static final String TAG = "SystemUiSettings";
 
+    // LED customization
+    private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
+
+    // LED customization
+    private PreferenceScreen mNotificationPulse;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.system_ui_settings);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        // LED customization
+        boolean hasNotificationLed = getResources().getBoolean(
+                com.android.internal.R.bool.config_intrusiveNotificationLed);
+        mNotificationPulse = (PreferenceScreen) findPreference(KEY_NOTIFICATION_PULSE);
+        if (!hasNotificationLed) {
+            getPreferenceScreen().removePreference(mNotificationPulse);
+        }
     }
 
     @Override
